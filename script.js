e2a29294393f48bad869cd947f84725ad0f4b425
Elayaraman-Ramalingam <116268532+Elayaraman-Ramalingam@users.noreply.github.com>
2023-10-31 14:58:13 +0530

    first commit

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// const carousel = document.querySelector(".marquee-container");
+// const child1 = document.querySelector(".marquee");
+// const child2 = document.querySelector(".marquee2");
+
+// function removeAndAppend() {
+//   const firstChild = carousel.firstElementChild;
+//   carousel.removeChild(firstChild); 
+//   carousel.insertBefore(firstChild, child2.nextSibling);
+// }
+
+// setInterval(removeAndAppend, 16000);
